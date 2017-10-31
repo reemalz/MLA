d00bee097357cfd7dfc4ba82007db2843cf20a51
Wejdan11 <32482846+Wejdan11@users.noreply.github.com>
2017-10-31 13:50:31 +0300

    wejdan

---
+++ MLA1/MoviePageViewController.swift
@@ -0,0 +1,52 @@
+//
+//  MoviePageViewController.swift
+//  MLA1
+//
+//  Created by user2 on ٩ صفر، ١٤٣٩ هـ.
+//  Copyright © ١٤٣٩ هـ njoool . All rights reserved.
+//
+
+import UIKit
+
+class MoviePageViewController: UIViewController {
+    
+   
+    @IBOutlet var RatingStars: [UIButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func RateMovie(_ sender: UIButton) {
+        var rate = NSInteger()
+        rate = sender.tag
+        
+        for star in RatingStars {
+            if star.tag<=rate{
+                star .setTitle("★", for: UIControlState.normal )}
+            else{
+                star .setTitle("☆", for: UIControlState.normal )}
+        }
+    }
+}
+/*
+ 
+ 
+ 
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destinationViewController.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
